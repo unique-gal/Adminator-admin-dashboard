ae7e3186d295239c3c156a7f7c9232253c454239
uniquegal <opss2976@gmail.com>
2020-05-19 21:08:34 +0900

    [ 갈유나 수정 ]

--- webpack/plugins/htmlPlugin.js
+++ webpack/plugins/htmlPlugin.js
@@ -3,7 +3,8 @@ const path = require("path"),
   HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const titles = {
-  Notice: "Notice",
+  index: "index",
+  notice: "Notice",
   calendar: "Calendar",
   validMember: "validMember",
   invalidMember: "invalidMember",
@@ -13,27 +14,12 @@ const titles = {
   lessonManager: "lessonManager",
   lessonSchedule: "lessonSchedule",
   Setting: "Setting",
-  datatable: "Datatable",
-
-  // 'index': 'Dashboard',
-  // 'blank': 'Blank',
-  // 'charts': 'Charts',
-  // 'chat': 'Chat',
-  // 'compose': 'Compose',
-  // 'email': 'Email',
-  // 'forms': 'Forms',
-  // 'google-maps': 'Google Maps',
-  // 'signin': 'Signin',
-  // 'signup': 'Signup',
-  // 'ui': 'UI',
-  // 'vector-maps': 'Vector Maps',
-  // '404': '404',
-  // '500': '500',
-  // 'basic-table': 'Basic Table',
+  CoachData: "CoachData",
 };
 
 module.exports = Object.keys(titles).map((title) => {
   return new HtmlWebpackPlugin({
+    //template: path.join(manifest.paths.src, `${title}.html`),
     template: path.join(manifest.paths.src, `${title}.html`),
     path: manifest.paths.build,
     filename: `${title}.html`,
